df4b96145d96caa42dfd1eec4978d16dc14a31b2
Soul <tsy_1213@hotmail.com>
2023-07-04 16:24:46 +0800

    Commit 04Jul2023

---
+++ src/com/soul/client/TestClient.java
@@ -0,0 +1,14 @@
+package com.soul.client;
+
+public class TestClient {
+
+	public TestClient() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
